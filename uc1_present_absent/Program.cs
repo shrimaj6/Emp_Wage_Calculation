3c1fdf941803c2558553d324fd7b02299226c464
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-03 16:17:36 +0530

    empwage

--- uc1_present_absent/Program.cs
+++ uc1_present_absent/Program.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 
-namespace uc2_dailywage
+namespace UC_3PartTime
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            int Is_Full_Time = 1;
-            int EMP_RATE_PER_HR = 20;
-
-            int empHrs = 0;
-            int empWage = 0;
-
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 2);
-
-            if (empCheck == Is_Full_Time)
+            static void Main(string[] args)
             {
-                empHrs = 8;
+                //Constatnts
+                int IS_PART_TIME = 1;
+                int IS_FULL_TIME = 2;
+                int EMP_WAGE_PER_HOUR = 20;
+                //Variable
+                int empHrs = 0;
+                int empWage = 0;
+                Random random = new Random();
+                //Computation
+                int empCheck = random.Next(0, 3);
+                if (empCheck == IS_PART_TIME)
+                {
+                    empHrs = 4;
+                }
+                else if (empCheck == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                }
+                else
+                {
+                    empHrs = 0;
+                }
+                empWage = empHrs * EMP_WAGE_PER_HOUR;
+                Console.WriteLine("Emp wage : " + empWage);
             }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("empWage : " +empWage);
-
         }
     }
-}
