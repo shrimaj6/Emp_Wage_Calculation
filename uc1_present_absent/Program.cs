a5cd67d443526c3bac7ce79469a35d85bb706183
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-03 15:32:13 +0530

    empwagecalc

--- uc1_present_absent/Program.cs
+++ uc1_present_absent/Program.cs
@@ -1,38 +1,32 @@
 ﻿using System;
 
-namespace uc1_present_absent
+namespace uc2_dailywage
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int Is_Full_Time = 1;
+            int EMP_RATE_PER_HR = 20;
 
-  
-        
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Is_Full_Time)
             {
-                int IsFullTime = 1;
-
-                Random random = new Random();
-
-                int empCheck = random.Next(0, 2);
-
-                if (empCheck == IsFullTime)
-                {
-                    Console.WriteLine("Employee is present");
-
-                }
-
-                else
-                {
-                    Console.WriteLine("Employee is absent");
-
-                }
-
-
+                empHrs = 8;
             }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("empWage : " +empWage);
+
         }
-
-
     }
-
 }
