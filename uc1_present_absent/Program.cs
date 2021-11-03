b5cc068e41dac0dba3bd783b0631e59fdf896acb
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-03 15:03:44 +0530

    empwagecomputation

---
+++ uc1_present_absent/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uc1_present_absent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to emp_wage_computation");
+            Console.ReadKey();
+        }
+    }
+
+}
